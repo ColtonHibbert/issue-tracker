7b922ddd4a778dde14495a31042d29958e4e2f5c
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-02-19 18:56:50 -0700

    db created and insertion ready for testing

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,7 @@ var expect = require('chai').expect;
 
 //const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function (app, db) {
 
   app.route('/api/issues/:project')
   
@@ -27,16 +27,32 @@ module.exports = function (app) {
       console.log('get body', req.body);
     })
     
-    .post(function (req, res){
+    .post(async function (req, res){
       console.log('post params', req.params);
       console.log('post query', req.query);
       console.log('post body', req.body);
       var project = req.params.project;
+      //err handling for missing required fields?
       const issueTitle = req.query.issue_title;
       const issueText = req.query.issue_text;
       const createdBy = req.query.created_by;
-      const assignedTo = req.query.assigned_to;
-      const statusText = req.query.status_text;
+      let assignedTo = req.query.assigned_to;
+      let statusText = req.query.status_text;
+      if(assignedTo === undefined) {
+        assignedTo = "";
+      }
+      if(statusText === undefined) {
+        statusText = "";
+      }
+      db.transaction(trx => {
+        trx.insert({
+          issue_title: issueTitle,
+          issue_text: issueText,
+          created_by: createdBy,
+          assigned_to: assignedTo,
+          status_text: statusText 
+        }).into('issue')
+      })
     })
     
     .put(function (req, res){
