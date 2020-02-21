76b3c00b8629eb974fdf0244d6a503f89c2bb926
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-02-20 20:38:52 -0700

    working on insertion if project doesn't exist

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ module.exports = function (app, db) {
       var project = req.params.project;
       var query = req.query;
       //console.log('get project', project)
-      console.log('get params', params);
+      console.log('get params', req.params);
       console.log('get query', query);
       console.log('get body', req.body);
     })
@@ -32,33 +32,59 @@ module.exports = function (app, db) {
       console.log('post query', req.query);
       console.log('post body', req.body);
       var project = req.params.project;
-      //err handling for missing required fields?
+      //err handling for missing required fields? none for this project, prevented on client side
       const issueTitle = req.body.issue_title;
       const issueText = req.body.issue_text;
       const createdBy = req.body.created_by;
       let assignedTo = req.body.assigned_to;
       let statusText = req.body.status_text;
       let projectId = null;
+      let projectFound = true;
+
       if(assignedTo === undefined) {
         assignedTo = "";
       }
       if(statusText === undefined) {
         statusText = "";
       }
-      await db.transaction(trx => {
-        trx.insert({
-          project_name: project
-        })
-        .into('project')
-        .returning('_id')
-        .then(data => {
-          projectId = data[0];
-          console.log(data[0], '_id for project')
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
+      await db.select('project_name').from('project').where('project_name', '=', project)
+      .returning('*')
+      .then( data => {
+        console.log(data[0], 'data')
+        if(data[0] === undefined ) {
+          console.log(data, 'data and projectFound should be false')
+          projectFound = false;
+        }
+        if(data[0]._id) {
+          projectId = data[0]._id
+        }
       })
       .catch(err => console.log(err))
+      
+      function insertProjectIfNeeded() {
+        if(projectFound === false) {
+          console.log('start of insertProjectifNeeded')
+          return db.transaction(trx => {
+            trx.insert({
+              project_name: project
+            })
+            .into('project')
+            .returning('_id')
+            .then(data => {
+              projectId = data[0];
+              console.log(data[0], '_id for project, inserting project')
+            })
+            .then(trx.commit)
+            .then(() => {
+              return
+            })
+            .catch(trx.rollback)
+          })
+          .catch(err => console.log(err))
+        }
+      }
+      await insertProjectIfNeeded();
+      console.log('after insertProjectIfNeeded')
 
       await db.transaction(trx => {
         trx.insert({
@@ -76,6 +102,7 @@ module.exports = function (app, db) {
         .catch(trx.rollback)
       })
       .catch(err => console.log(err))
+
     })
     
     .put(function (req, res){
